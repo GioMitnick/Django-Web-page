50c93ea5bc898419118c8d258574dda36a5c57d3
luzGS03 <lgrandes2200@alumno.ipn.mx>
2024-06-21 15:10:05 -0600

    prueba

--- Productos/urls.py
+++ Productos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from. import views
 
 urlpatterns=[ path ('', views.principal),
-             path('Productos/',views.Productos),
-             path('info/', views.info_productos)]
+             path('Productos',views.Productos),
+             path('info/<int:id>', views.info_productos)]
