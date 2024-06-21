a77b4e30652b51f94dae1b978547109b10f5d673
StroperFlame <tecmac2002@gmail.com>
2024-06-20 22:51:06 -0600

    Edit views.py

--- Productos/views.py
+++ Productos/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .models import Producto
+from Productos.models import Producto
 
 # Create your views here.
-def info_productos(request):
-    ps=pr.objects.all()
-    return render (request, 'productos.html',{'Producto':ps})
+#def info_productos(request):
+#   ps=pr.objects.all()
+#  return render (request, 'productos.html',{'Producto':ps})
+
+def index(request):
+    pr=Producto.objects.all()
+    return render(request, "productos.html", {"Producto":Producto} )
